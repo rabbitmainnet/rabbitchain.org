440170fb7aaaa0e9ccc563f34cebb501b98aae7e
Lust Chain <contact@lustchain.org>
2026-09-04 12:53:13 -0300

    release: publish Rabbit Core Testnet V2.1

--- src/config/release.js
+++ src/config/release.js
@@ -1,11 +1,11 @@
 import { NETWORKS } from './networks'
 
-export const RELEASE_TAG = 'rabbit-core-testnet-v2'
-export const RELEASE_COMMIT = '302b8b27a16e943a216a7403b017e2397ea15664'
+export const RELEASE_TAG = 'rabbit-core-testnet-v2.1'
+export const RELEASE_COMMIT = '6ec93b2195d4f5bba2c0fcada2111ddb019c84e2'
 export const RELEASE_URL =
-  'https://github.com/rabbitmainnet/rabbit-geth/releases/tag/rabbit-core-testnet-v2'
+  'https://github.com/rabbitmainnet/rabbit-geth/releases/tag/rabbit-core-testnet-v2.1'
 export const RELEASE_DOWNLOAD_BASE =
-  'https://github.com/rabbitmainnet/rabbit-geth/releases/download/rabbit-core-testnet-v2'
+  'https://github.com/rabbitmainnet/rabbit-geth/releases/download/rabbit-core-testnet-v2.1'
 
 export const DOWNLOADS = [
   {
@@ -13,16 +13,16 @@ export const DOWNLOADS = [
     platform: 'Windows',
     architecture: 'AMD64',
     format: 'ZIP',
-    filename: 'rabbit-core-testnet-v2-windows-amd64.zip',
-    sha256: '6d55fdb00bc309370daf22a536c9812f63e12886148d6ef801e4094b618ecc4b',
+    filename: 'rabbit-core-testnet-v2.1-windows-amd64.zip',
+    sha256: '7d0f20e8cdb206bcff5e584a4f77572d366d89e3f966d9d84de835e5a228ffd9',
   },
   {
     key: 'linux-amd64',
     platform: 'Linux',
     architecture: 'AMD64',
     format: 'tar.gz',
-    filename: 'rabbit-core-testnet-v2-linux-amd64.tar.gz',
-    sha256: '3f88e4db6f193e8cda3bd4817a2d003336a64b0b71d6530b1e5167bc541aa74b',
+    filename: 'rabbit-core-testnet-v2.1-linux-amd64.tar.gz',
+    sha256: '8a9ce6b8ed2b3bb40add408a9cba89c42078f8409882a6579ec7e684bcd7d757',
   },
 ].map((download) => ({
   ...download,
